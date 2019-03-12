2b0e1ca9f5167ecd0f8a096bfbfac00f04c15973
UKHANZADA-NIS-P\ukhanzada <ukhanzada@nisum.com>
2019-03-12 18:47:13 +0500

    add ActivityIndicator support in button component.

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,18 +2,25 @@ import React  from 'react';
 import PropTypes from 'prop-types';
 import {
   View,
-  Text
+  Text,
+  ActivityIndicator
 } from 'react-native';
 import Touchable from 'react-native-platform-touchable';
 import {theme, THEME_CONFIG} from '../style';
 
-const Button = ({containerStyle, ripple, textStyle, text, icon, touchableProps}) => {
+const Button = ({containerStyle, ripple, textStyle, text, icon, loading, touchableProps}) => {
   // const {style, background, ...props} = touchableProps; //removed style and background from touchable props and pass manually by containerStyle and ripple.
   return (
-    <Touchable style={containerStyle} background={Touchable.Ripple(ripple)} {...touchableProps}>
+    <Touchable disabled={loading} style={[containerStyle, {opacity: loading ? 0.5 : 1}]} background={Touchable.Ripple(ripple)} {...touchableProps}>
       <View>
-        {icon}
-        <Text style={textStyle}>{text}</Text>
+        {
+          loading ?
+            <ActivityIndicator size="small" color={THEME_CONFIG.primaryTextColor} /> :
+            <View>
+              {icon}
+              <Text style={textStyle}>{text}</Text>
+            </View>
+        }
       </View>
     </Touchable>
   )
